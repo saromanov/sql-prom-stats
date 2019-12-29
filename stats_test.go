4f52d6379003f2c51647284da85a8a8a156da5cd
Sergey <xxsmotur@gmail.com>
2019-12-29 23:58:40 +0500

    Add new test for checking in use connections

--- stats_test.go
+++ stats_test.go
@@ -26,10 +26,19 @@ func idleConnectionQuery(t *testing.T, db *sql.DB) {
 	}
 }
 
+func leakConnectionsQuery(t *testing.T, db *sql.DB) {
+	for i := 0; i < 10; i++ {
+		_, err := db.Query("SELECT * FROM accounts")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
 func getStatNumber(s, prefix string) int {
 	lines := strings.Split(s, "\n")
 	for _, line := range lines {
-		if strings.HasPrefix(line, "database_sql_stats_golang_idle") {
+		if strings.HasPrefix(line, prefix) {
 			lastChar := line[len(line)-1 : len(line)]
 			res, _ := strconv.ParseInt(lastChar, 10, 32)
 			return int(res)
@@ -37,6 +46,23 @@ func getStatNumber(s, prefix string) int {
 	}
 
 	return 0
+}
+
+func makeRequest(url string) (string, error) {
+	rr, err := http.Get(fmt.Sprintf("%s/metrics", url))
+	if err != nil {
+		return "", err
+	}
+	if status := rr.StatusCode; status != http.StatusOK {
+		return "", fmt.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	body, err := ioutil.ReadAll(rr.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
 }
 
 func TestGetProjectsHandler(t *testing.T) {
@@ -51,22 +77,34 @@ func TestGetProjectsHandler(t *testing.T) {
 	srv := httptest.NewServer(promhttp.Handler())
 	defer srv.Close()
 	idleConnectionQuery(t, db)
-	rr, err := http.Get(fmt.Sprintf("%s/metrics", srv.URL))
-	if err != nil {
-		t.Fatal(err)
-	}
-	if status := rr.StatusCode; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	body, err := ioutil.ReadAll(rr.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	data := string(body)
+	data, err := makeRequest(srv.URL)
+	assert.NoError(t, err)
 	idle := getStatNumber(data, "database_sql_stats_golang_idle")
 	assert.NotEqual(t, -1, strings.Index(data, "database_sql_stats_golang_max_idle"))
 	assert.Equal(t, 1, idle)
 
+	db.Close()
+	data, err = makeRequest(srv.URL)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, getStatNumber(data, "database_sql_stats_golang_idle"))
+
+}
+
+func TestGetProjectsHandlerLeakConnections(t *testing.T) {
+	db, err := sql.Open("postgres", "postgres://pinger:pinger@localhost:5432/pinger")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	collector := NewSQLStats("db_name", db)
+
+	prometheus.MustRegister(collector)
+	srv := httptest.NewServer(promhttp.Handler())
+	defer srv.Close()
+	leakConnectionsQuery(t, db)
+	data, err := makeRequest(srv.URL)
+	assert.NoError(t, err)
+	inUse := getStatNumber(data, "database_sql_stats_golang_in_use")
+	assert.NotEqual(t, -1, strings.Index(data, "database_sql_stats_golang_in_use"))
+	assert.Equal(t, 10, inUse)
 }
